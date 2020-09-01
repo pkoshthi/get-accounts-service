fffa7a8cc4fecd2192f9231ddb761a5c5095f8c1
GANESH <GANESH@GANESH-PC>
2020-09-01 16:32:26 +0530

    Added comment in respository interface

--- GetAccountsService/src/main/java/com/accounts/repository/AccountRepository.java
+++ GetAccountsService/src/main/java/com/accounts/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.accounts.repository;
 import org.springframework.data.repository.CrudRepository;
 import com.accounts.model.Account;
 
+//creating a repository interface
 public interface AccountRepository extends CrudRepository<Account, Integer>
 {
 
